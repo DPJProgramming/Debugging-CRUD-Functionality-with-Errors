2ebc31faeec76a6a4862c4c08305c1fec79b9657
cheesy d <cheesyd64@hotmail.com>
2023-03-02 18:13:45 -0800

    Messages work

--- CPW219-CRUD-Troubleshooting/Controllers/StudentsController.cs
+++ CPW219-CRUD-Troubleshooting/Controllers/StudentsController.cs
@@ -33,11 +33,11 @@ namespace CPW219_CRUD_Troubleshooting.Controllers
             {
                 StudentDb.Add(p, context); 
                 ViewData["Message"] = $"{p.Name} was added!";
-                return View(p); 
+                return View(); 
             }
 
             //Show web page with errors
-            return View(p);
+            return View();
         }
 
         [HttpGet]
@@ -56,11 +56,11 @@ namespace CPW219_CRUD_Troubleshooting.Controllers
             if (ModelState.IsValid)
             {
                 StudentDb.Update(context, p);
-                ViewData["Message"] = "Product Updated!";
-                return View(p);
+                ViewData["Message"] = $"{p.Name} Updated!";
+                return View();
             }
             //return view with errors
-            return View(p);
+            return View();
         }
 
         [HttpGet]
